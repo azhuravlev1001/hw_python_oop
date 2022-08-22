720b5d7f3fd17211fb45c3b80edfc5260751b2ae
Алексей Журавлев <azhuravlev1001@gmail.com>
2022-08-22 17:17:05 +0300

    испр по замечаниям, пройден тест

--- homework.py
+++ homework.py
@@ -3,13 +3,8 @@ class InfoMessage:
     MES_TEMPL = ('Тип тренировки: {0}; Длительность: {1:.3f} ч.; '
                  'Дистанция: {2:.3f} км; Ср. скорость: {3:.3f} км/ч; '
                  'Потрачено ккал: {4:.3f}.')
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float
-                 ) -> None:
+    def __init__(self,training_type: str, duration: float,
+                 distance: float, speed: float, calories: float) -> None:
 
         self.training_type = training_type
         self.duration = duration
